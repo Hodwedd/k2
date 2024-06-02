8425fc3f9e5415c875f402948d87bcfb41c99b04
megli <megli@Marco>
2024-06-02 17:19:32 +0200

    id-5 unresolved

---
+++ src/main/java/control/CSPFilter.java
@@ -0,0 +1,26 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CSPFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Content-Security-Policy", "frame-ancestors 'self'");
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+    }
+}
